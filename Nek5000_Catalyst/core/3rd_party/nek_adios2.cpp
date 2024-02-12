776b3047117ed161e8b1c7e3efbe63b571b02967
JudithYi <judith.yi.ju@gmail.com>
2024-02-12 09:18:43 +0100

    missing reading

--- Nek5000_Catalyst/core/3rd_party/nek_adios2.cpp
+++ Nek5000_Catalyst/core/3rd_party/nek_adios2.cpp
@@ -70,14 +70,14 @@ void adios_writer_init(
     const int type_index
 ){  
     if(adios_init == 0){
+	startTotal = std::clock();
+	std::string configFile="config/config.xml";
         adios = adios2::ADIOS(configFile, comm);
-	    io = adios.DeclareIO("writer");
+    	io = adios.DeclareIO("writer");
         MPI_Comm_rank(comm, &rank);
         MPI_Comm_size(comm, &size);
     }
     if(writer_num[type_index] == 0){
-	    startTotal = std::clock();
-        std::string configFile="config/config.xml";
         MPI_Comm_size(worldComm, &world_size[type_index]);
         // int	worldComm_f = MPI_Comm_c2f(worldComm);
         // int comm_f = MPI_Comm_c2f(comm);
@@ -117,7 +117,7 @@ extern "C" void adios2_setup_async_(
     const double *w,
     const double *tem,
     const double *bm,
-    const int *comm_int
+    const int *comm_int,
     const int *type_idx
 ){
     //writer0.BeginStep();
@@ -291,9 +291,10 @@ extern "C" void adios2_update_lossy_(
     const double *u,
     const double *w,
     const double *temp,
-    const int* type_index
+    const int* type_idx
 ){
     startT = std::clock();
+    int type_index = * type_idx;
     ++insituCount[type_index];
     int idx_writer = insituCount[type_index]%writer_num[type_index];
     writer[type_index][idx_writer].BeginStep();
@@ -307,4 +308,210 @@ extern "C" void adios2_update_lossy_(
     writer[type_index][idx_writer].Put<int>(InsituCounter, vInSituCounter.data());
     writer[type_index][idx_writer].EndStep();
     dataTime += (std::clock() - startT) / (double) CLOCKS_PER_SEC;
+}
+
+extern "C" void adios2_read_(
+    int *lglelrr,
+    double *pr,
+    double *v,
+    double *u,
+    double *w,
+    //const double *temp,
+    const int *nval,
+    const int *nelvin,
+    const int *nelb,
+    const int *nelgv,
+    const int *nelgt,
+    const int *comm_int,
+    char *fname
+){
+    startT = std::clock();
+    std::string configFile="config/config.xml";
+    MPI_Comm comm = MPI_Comm_f2c(*comm_int);
+    //adios = adios2::ADIOS(configFile, comm);
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+    // In config, writer is opening the insituMPI engine.
+    // I guess this is due to the fact that this is just a write
+    // to let the compressor executable access the data.
+//    Coment all concerning ior since it was declared in the setup
+//    ior = adios.DeclareIO("inputReader");
+//    // In config, writer 0 uses the BPfile engine, wich i guess stands for//
+//    // bzip2
+//    if (!ior.InConfigFile())
+//    {
+//        // if not defined by user, we can change the default settings
+//        // BPFile is the default engine
+//        ior.SetEngine("BPFile");
+//        ior.SetParameters({{"num_threads", "1"}});
+//
+//        // ISO-POSIX file output is the default transport (called "File")
+//        // Passing parameters to the transport
+//    }
+    // Understand how the elements are divided among ranks.
+    // set the pointers to the starting points in vectors of for each rank.
+    //unsigned int nelv = static_cast<unsigned int>((*nelgv) / size);
+    unsigned int nelv = static_cast<unsigned int>((*nelvin));
+    unsigned int start = static_cast<unsigned int>(*nelb);
+    //if((*nelgv)%size != 0){
+    //    if(rank < ((*nelgv) % size)){
+    //        ++nelv;
+    //        start += static_cast<unsigned int>(rank);
+    //    }else{
+    //        start += static_cast<unsigned int>((*nelgv)%size);
+    //    }
+    //}
+    start *= static_cast<unsigned int>(*nval);
+    // n is count, i.e number of entries in the array in my rank
+    unsigned int n = static_cast<unsigned int> (*nval) * nelv;
+    // gn is the total size of the arrays, not per io rank
+    unsigned int gn = static_cast<unsigned int>((*nelgv)*(*nval));
+
+    // These are the indicators for the global indices
+    unsigned int nelv3 = static_cast<unsigned int>((*nelvin));
+    unsigned int start3 = static_cast<unsigned int>(*nelb);
+    unsigned int n3 = static_cast<unsigned int> (nelv);
+    unsigned int gn3 = static_cast<unsigned int>((*nelgv));
+
+    if (rank==0){
+
+    std::cout << "what adios is getting:"<< std::endl;
+    std::cout << "nvals:"<< static_cast<unsigned int> (*nval) << std::endl;
+    std::cout << "nelgv:"<< static_cast<unsigned int> (*nelgv) << std::endl;
+    std::cout << "nelgt:"<< static_cast<unsigned int> (*nelgt) << std::endl;
+
+    std::cout << "what adios is calculating:"<< std::endl;
+    std::cout << "nelv:"<< nelv << std::endl;
+    std::cout << "start:"<< start << std::endl;
+    std::cout << "count:"<< n << std::endl;
+    std::cout << "total number of entries:"<< gn << std::endl;
+
+    }
+    //std::cout << "rank"<< rank << ": " << gn << ", " << start << "," << n << std::endl;
+
+    //std::cout << "Reading the file " << std::endl;
+
+    // This file is here just to correctly advance the reader
+    // I have an error if I send the name from fortram. I would need to trim the string.
+    ifile=ifile+1;
+    // Make a new name every time the reader is called
+    std:: string fileName= "out.f0000"+ std::to_string(ifile) +".bp";
+    // Read the new name
+    adios2::Engine readr = ior.Open(fileName,adios2::Mode::Read,comm);
+
+    //adios2::Engine readr = ior.Open("out.f00001.bp",adios2::Mode::Read,comm);
+    //adios2::Engine readr = ior.Open(fname,adios2::Mode::Read,comm);
+    int step = 1;
+    bool firstStep = true;
+
+    // I am commenting the while loop and all the breaks since they
+    // appear to create problems when reading multiple files in succession.
+    // the problem is somewhere in the "steps"
+
+    //while (true)
+    //{
+     	//adios2::StepStatus status =
+        //readr.BeginStep(adios2::StepMode::Read);
+    //    if (status != adios2::StepStatus::OK)
+    //    {
+    //        break;
+    //    }
+
+        vxr = ior.InquireVariable<double>("VX_OUT");
+        if (!vxr)
+        {
+            std::cout << "Error: NO variable VX_OUT found. Unable to proceed. " << std::endl;
+    //        break;
+        }
+
+        vyr = ior.InquireVariable<double>("VY_OUT");
+        if (!vyr)
+        {
+            std::cout << "Error: NO variable VY_OUT found. Unable to proceed. " << std::endl;
+    //        break;
+        }
+
+        vzr = ior.InquireVariable<double>("VZ_OUT");
+        if (!vzr)
+        {
+            std::cout << "Error: NO variable VZ_OUT found. Unable to proceed. " << std::endl;
+    //        break;
+        }
+
+        prr = ior.InquireVariable<double>("P_OUT");
+        if (!prr)
+        {
+            std::cout << "Error: NO variable P_OUT found. Unable to proceed. " << std::endl;
+    //        break;
+        }
+        lglelr = ior.InquireVariable<int>("LGLEL_OUT");
+        if (!lglelr)
+        {
+            std::cout << "Error: NO variable LGLEL_OUT found. Unable to proceed. " << std::endl;
+    //        break;
+        }
+
+        if (firstStep)
+        {
+        	// Promise that we are not going to change the variable sizes
+                // nor add new variables
+                readr.LockReaderSelections();
+
+		vVXr.resize(n);
+		vVYr.resize(n);
+		vVZr.resize(n);
+		vVPrr.resize(n);
+		vVLGLELr.resize(n3);
+
+		firstStep=false;
+        }
+
+
+        // std::cout << "Select how much is yours "
+           //               << std::endl;
+        vxr.SetSelection({{start}, {n}});
+        vyr.SetSelection({{start}, {n}});
+        vzr.SetSelection({{start}, {n}});
+        prr.SetSelection({{start}, {n}});
+        lglelr.SetSelection({{start3}, {n3}});
+
+
+        // std::cout << "get the data "
+           //              << std::endl;
+        readr.Get<double>(vxr,vVXr.data());
+        readr.Get<double>(vyr,vVYr.data());
+        readr.Get<double>(vzr,vVZr.data());
+        readr.Get<double>(prr,vVPrr.data());
+        readr.Get<int>(lglelr,vVLGLELr.data());
+        //readr.Get<double>(vzr,w);
+
+        //readr.EndStep();
+        //++step;
+    //}
+    readr.Close();
+    //++step;
+
+    //copy the data into w
+    if (rank==0){
+    std::cout <<"Copying data into nek vector " << std::endl;
+    }
+    //if (rank==0){
+    for (int i=0; i<vVZr.size(); i++){
+    //    std::cout <<"entry "<< i <<"is " << vVZr[i] << " ";
+    //    std::cout <<"size of read vector " <<"is " << vvzr.size() << std::endl;
+    pr[i]=vVPrr[i];
+    v[i]=vVXr[i];
+    u[i]=vVYr[i];
+    w[i]=vVZr[i];
+    }
+    for (int i=0; i<vVLGLELr.size(); i++){
+    //    std::cout <<"entry "<< i <<"is " << vVZr[i] << " ";
+    //    std::cout <<"size of read vector " <<"is " << vvzr.size() << std::endl;
+    lglelrr[i]=vVLGLELr[i];
+    }
+    //}
+
+    dataTime += (std::clock() - startT) / (double) CLOCKS_PER_SEC;
+    //}
+
 }
